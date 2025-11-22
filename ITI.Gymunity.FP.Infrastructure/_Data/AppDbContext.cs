37a282f8b7a7d24a2c2ebf634ae0d00045c3b79f
Mohmed Kamal <mohamed.elsharif022@gmail.com>
2025-11-22 18:31:50 +0200

    Add project files.

---
+++ ITI.Gymunity.FP.Infrastructure/_Data/AppDbContext.cs
@@ -0,0 +1,71 @@
+﻿using ITI.Gymunity.FP.Domain.Entities;
+using ITI.Gymunity.FP.Domain.Entities.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI.Gymunity.FP.Infrastructure._Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+        // Add DbSet properties for your entities here
+
+        // Configure the model
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+
+            SetGlobalQueryFilter<BaseEntity>(modelBuilder, e => !e.IsDeleted);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+
+            AppContextSeed.SeedDatabase(modelBuilder);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var now = DateTimeOffset.UtcNow;
+            foreach (var entry in ChangeTracker.Entries<BaseEntity>())
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Entity.CreatedAt = now;
+                    entry.Entity.UpdatedAt = now;
+                }
+                else if (entry.State == EntityState.Modified)
+                {
+                    entry.Entity.UpdatedAt = now;
+                }
+                else if (entry.State == EntityState.Deleted)
+                {
+                    entry.State = EntityState.Modified;
+                    entry.Entity.IsDeleted = true;
+                    entry.Entity.UpdatedAt = now;
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        private void SetGlobalQueryFilter<TBase>(ModelBuilder modelBuilder, Expression<Func<TBase, bool>> filter)
+        {
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                if (typeof(TBase).IsAssignableFrom(entityType.ClrType))
+                {
+                    var parameter = Expression.Parameter(entityType.ClrType);
+                    var body = ReplacingExpressionVisitor.Replace(filter.Parameters[0], parameter, filter.Body);
+                    var lambda = Expression.Lambda(body, parameter);
+
+                    entityType.SetQueryFilter(lambda);
+                }
+            }
+        }
+    }
+}
