2c81813872df83cb929f7b5d5b47fdd7fbf0ae46
Mohmed Kamal <mohamed.elsharif022@gmail.com>
2025-12-01 20:03:20 +0200

    Added Areas To the project

---
+++ ITI.Gymunity.FP.APIs/Areas/Admin/AdminBaseController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITI.Gymunity.FP.APIs.Areas.Admin
+{
+    [Route("api/admin/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "Admin")]
+    public abstract class AdminBaseController : ControllerBase
+    {
+    }
+}
