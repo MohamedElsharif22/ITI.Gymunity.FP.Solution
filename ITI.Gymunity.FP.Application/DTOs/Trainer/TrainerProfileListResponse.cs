a79721524fb6470786af2e240aaee6222b11b96b
medo0 <medo0@DESKTOP-E4DI20Q>
2025-12-05 16:06:14 +0200

    trainer profile endpoints

---
+++ ITI.Gymunity.FP.Application/DTOs/Trainer/TrainerProfileListResponse.cs
@@ -0,0 +1,21 @@
+﻿namespace ITI.Gymunity.FP.Application.DTOs.Trainer
+{
+    // Response for list endpoints - excludes status fields
+    public class TrainerProfileListResponse
+    {
+        public int Id { get; set; }
+        public DateTimeOffset? UpdatedAt { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public string UserName { get; set; } = null!;
+        public string Handle { get; set; } = null!;
+        public string Bio { get; set; } = string.Empty;
+        public string? CoverImageUrl { get; set; }
+        public string? VideoIntroUrl { get; set; }
+        public string? BrandingColors { get; set; }
+        public bool IsVerified { get; set; }
+        public DateTime? VerifiedAt { get; set; }
+        public decimal RatingAverage { get; set; }
+        public int TotalClients { get; set; }
+        public int YearsExperience { get; set; }
+    }
+}
--- ITI.Gymunity.FP.Application/Mapping/MappingProfile.cs
+++ ITI.Gymunity.FP.Application/Mapping/MappingProfile.cs
@@ -1,9 +1,14 @@
 ﻿using AutoMapper;
+<<<<<<< Updated upstream
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+=======
+using ITI.Gymunity.FP.Application.DTOs.Trainer;
+using ITI.Gymunity.FP.Domain.Models.Trainer;
+>>>>>>> Stashed changes
 
 namespace ITI.Gymunity.FP.Application.Mapping
 {
@@ -11,7 +16,30 @@ namespace ITI.Gymunity.FP.Application.Mapping
     {
         public MappingProfile()
         {
+<<<<<<< Updated upstream
             // Create your mappings here
+=======
+            // TrainerProfile to List Response (excludes status fields)
+            CreateMap<TrainerProfile, TrainerProfileListResponse>()
+                .ForMember(dest => dest.UserName, opt => opt.MapFrom(tp => tp.User.UserName));
+
+            // TrainerProfile to Detail Response (includes status fields)
+            CreateMap<TrainerProfile, TrainerProfileDetailResponse>()
+                .ForMember(dest => dest.UserName, opt => opt.MapFrom(tp => tp.User.UserName));
+
+            // Create request to TrainerProfile
+            CreateMap<CreateTrainerProfileRequest, TrainerProfile>()
+                .ForMember(dest => dest.CoverImageUrl, opt => opt.Ignore())
+                .ForMember(dest => dest.StatusImageUrl, opt => opt.Ignore())
+                .ForMember(dest => dest.StatusDescription, opt => opt.Ignore());
+
+            // Update request to TrainerProfile (only maps non-null values)
+            CreateMap<UpdateTrainerProfileRequest, TrainerProfile>()
+                .ForMember(dest => dest.CoverImageUrl, opt => opt.Ignore())
+                .ForMember(dest => dest.StatusImageUrl, opt => opt.Ignore())
+                .ForMember(dest => dest.StatusDescription, opt => opt.Ignore())
+                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+>>>>>>> Stashed changes
         }
     }
 }
