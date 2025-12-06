6a40621e23fcea5be71af509c5ffef5b385dc338
medo0 <medo0@DESKTOP-E4DI20Q>
2025-12-06 17:44:51 +0200

    fix trainer profile injection issue

---
+++ ITI.Gymunity.FP.Application/Specefications/TrainerWithUsersAndProgramsSpecs.cs
@@ -0,0 +1,24 @@
+﻿using ITI.Gymunity.FP.Domain.Models.Trainer;
+using ITI.Gymunity.FP.Domain.Specification;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace ITI.Gymunity.FP.Application.Specefications
+{
+    internal class TrainerWithUsersAndProgramsSpecs : BaseSpecification<TrainerProfile>
+    {
+        public TrainerWithUsersAndProgramsSpecs()
+        {
+            AddInclude(t => t.User);
+            //AddInclude(tp => tp.Programs);
+            AddInclude(q => q.Include(tp => tp.Programs).ThenInclude(p => p.Weeks));
+        }
+
+        public TrainerWithUsersAndProgramsSpecs(Expression<Func<TrainerProfile, bool>>? criteria) : base(criteria)
+        {
+            AddInclude(t => t.User);
+            //AddInclude(tp => tp.Programs);
+            AddInclude(q => q.Include(tp => tp.Programs).ThenInclude(p => p.Weeks));
+        }
+    }
+}
