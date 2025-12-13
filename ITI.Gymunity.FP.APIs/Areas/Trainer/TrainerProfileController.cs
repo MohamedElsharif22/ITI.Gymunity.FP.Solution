c33fdfb1ddeef66025ffa1403f6b837fe045fa42
Ahmed Refaat <ahmedr162018010@gmail.com>
2025-12-13 11:34:21 +0200

    subscribe

--- ITI.Gymunity.FP.APIs/Areas/Trainer/TrainerProfileController.cs
+++ ITI.Gymunity.FP.APIs/Areas/Trainer/TrainerProfileController.cs
@@ -17,11 +17,43 @@ namespace ITI.Gymunity.FP.APIs.Areas.Trainer
         {
             var profiles = await _trainerProfileService.GetAllProfiles();
 
-            if(!profiles.Any())
+            if (!profiles.Any())
                 return new ObjectResult(new ApiResponse(400, "No trainer profiles found."));
 
             return new OkObjectResult(profiles);
         }
+
+        // Get TrainerProfile By id  
+
+        [HttpGet("GetById/{id:int}")]
+        [ProducesResponseType(typeof(TrainerProfileResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var profile = await _trainerProfileService.GetProfileById(id);
+
+            if (profile is null)
+                return NotFound(new ApiResponse(404, "Trainer profile not found."));
+
+            return Ok(profile);
+        }
+
+        [HttpPost("Create")]
+        [ProducesResponseType(typeof(TrainerProfileResponse), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Create([FromBody] CreateTrainerProfileRequest request)
+        {
+            try
+            {
+                var created = await _trainerProfileService.CreateProfileAsync(request);
+                return CreatedAtAction(nameof(GetById), new { id = created!.Id }, created);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ApiResponse(400, ex.Message));
+            }
+        }
+
 
     }
 }
---
+++ ITI.Gymunity.FP.Application/DTOs/User/Subscribe/SubscribePackageRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI.Gymunity.FP.Application.DTOs.User.Subscribe
+{
+    public class SubscribePackageRequest
+    {
+        public int PackageId { get; set; }
+
+    }
+}
