bb8f598d898d6af236689e421357dbc2a28956d1
Amr Abdellah Kasem <amrkasem047@gmail.com>
2025-12-20 18:58:00 +0200

    remove AllChat

--- ChatBackup/Chat/StartThreadRequest.cs
+++
@@ -1,9 +0,0 @@
-namespace ITI.Gymunity.FP.Application.DTOs.Chat
-{
- public class StartThreadRequest
- {
- public string TrainerId { get; set; } = null!;
- public string ClientId { get; set; } = null!;
- public string InitialMessage { get; set; } = string.Empty;
- }
-}
