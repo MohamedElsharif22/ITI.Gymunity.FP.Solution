12d9b8c7623df39c421700458fe8fb3dac22f133
Mohamed Wahed <medo03459@gmail.com>
2026-01-01 00:31:09 +0200

    Add trainer profile endpoints and subscription updates

---
+++ ITI.Gymunity.FP.Infrastructure/_Data/Migrations/20251231210319_ggggggg.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ITI.Gymunity.FP.Infrastructure._Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ggggggg : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
