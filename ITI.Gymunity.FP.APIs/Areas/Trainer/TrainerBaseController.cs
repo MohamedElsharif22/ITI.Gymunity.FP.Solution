b874305c5da5281c2b92549d6d074df87bd19290
Mohmed Kamal <mohamed.elsharif022@gmail.com>
2026-01-01 01:03:57 +0200

    Trainer updates

--- ITI.Gymunity.FP.APIs/Areas/Trainer/TrainerBaseController.cs
+++ ITI.Gymunity.FP.APIs/Areas/Trainer/TrainerBaseController.cs
@@ -8,10 +8,18 @@ namespace ITI.Gymunity.FP.APIs.Areas.Trainer
 {
     [Route("api/trainer/[controller]")]
     [ApiController]
-    [AllowAnonymous] // Temporary for testing purposes
-    //[Authorize(Roles = "Trainer")] // Uncomment when authentication is ready
+    [Authorize(Roles = "Trainer")] // Uncomment when authentication is ready
 
     public class TrainerBaseController : ControllerBase
     {
+        protected int GetTrainerId()
+        {
+            // Try to read trainer id from claims (NameIdentifier expected to be trainer profile id or user id)
+            var idClaim = User?.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier)?.Value;
+            if (int.TryParse(idClaim, out var id)) return id;
+
+            // If not available or not int, return 0 as invalid
+            return 0;
+        }
     }
 }
